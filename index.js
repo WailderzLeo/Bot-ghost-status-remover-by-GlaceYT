7302b10243ecfd35c9edecb6f477919545405feb
WailderzLeo <wailderzleo@gmail.com>
2024-09-01 00:11:31 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to PS Void","Playing PS Void"];
 
 
 let currentIndex = 0;
